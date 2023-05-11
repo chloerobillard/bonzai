8741720bb08dc9bee4712d917df4dd5d6f09a025
Chloe Robillard <chloe@uni.minerva.edu>
2023-05-11 14:28:32 -0400

    Pushing Initial Python Structural Code

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+# from flask_login import LoginManager
+import os
+
+def create_app(test_config = None):
+    template_dir = os.path.abspath('frontend')
+    app = Flask(__name__, instance_relative_config = True, template_folder = template_dir, static_folder = template_dir, static_url_path = '')
+    app.config.from_mapping(SECRET_KEY = 'admin', DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'))
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent = True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    login_manager = LoginManager(app)
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user():
+        pass
+
+    return app
---
+++ unit_test/__init__.py
@@ -0,0 +1,16 @@
+from web import create_app
+import unittest
+import os
+import tempfile
+
+app = create_app()
+
+class UnitTest(unittest.TestCase):
+    def setup(self):
+        self.db, app.config['DATABASE'] = tempfile.mkstemp()
+        app.testing = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        os.close(self.db)
+        os.unlink(app.config['DATABASE'])
